fece1717b995aecbe61b920995163bb830eb405d
i3water <121024123@qq.com>
2018-03-06 23:58:12 +0800

    fix BLE bug

--- src/BlinkerSimpleESP32_BLE.h
+++ src/BlinkerSimpleESP32_BLE.h
@@ -7,8 +7,8 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 
-#define SERVICE_UUID        "0000ffe0-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "0000ffe1-36e1-4688-b7f5-ea07361b26a8"
+// #define SERVICE_UUID        "0000ffe0-1fb5-459e-8fcc-c5c9c331914b"
+// #define CHARACTERISTIC_UUID "0000ffe1-36e1-4688-b7f5-ea07361b26a8"
 
 class BlinkerTransportESP32_BLE
     :
@@ -25,11 +25,11 @@ class BlinkerTransportESP32_BLE
             BLEDevice::init("Blinker");
             pServer = BLEDevice::createServer();
 
-            pService = pServer->createService(SERVICE_UUID);
+            pService = pServer->createService(BLEUUID((uint16_t)0xffe0));//SERVICE_UUID
             pServer->setCallbacks(this);
 
             pCharacteristic = pService->createCharacteristic(
-                                            CHARACTERISTIC_UUID,
+                                            BLEUUID((uint16_t)0xffe1),//CHARACTERISTIC_UUID,
                                             BLECharacteristic::PROPERTY_READ |
                                             BLECharacteristic::PROPERTY_NOTIFY |
                                             BLECharacteristic::PROPERTY_WRITE_NR
@@ -50,11 +50,12 @@ class BlinkerTransportESP32_BLE
             macStr[0] = 0x48;
             macStr[1] = 0X4d;
             for (uint8_t cpynum = 0; cpynum < 6; cpynum++) {
-                macStr[cpynum + 2] = (char)ble_m_address[cpynum];
+                macStr[cpynum+2] = (char)ble_m_address[cpynum];
             }
 
             pAdvertisementData.setManufacturerData(macStr);
             pAdvertising->setAdvertisementData(pAdvertisementData);
+            pAdvertising->addServiceUUID(BLEUUID((uint16_t)0xffe0));
             pAdvertising->start();
         }
 
@@ -62,7 +63,7 @@ class BlinkerTransportESP32_BLE
         {
             if (isAvail) {
 #ifdef BLINKER_DEBUG_ALL
-                BLINKER_LOG2(BLINKER_F("handleBLE: "), BLEBuf);
+                BLINKER_LOG2(BLINKER_F("handleBLE: "), BLEBuf);//
 #endif
                 isAvail = false;
 
@@ -73,7 +74,7 @@ class BlinkerTransportESP32_BLE
             }
         }
 
-        String lastRead() { return STRING_format(BLEBuf);}
+        String lastRead() { return STRING_format(BLEBuf); }//
 
         void print(String s) {
 #ifdef BLINKER_DEBUG_ALL
@@ -102,6 +103,7 @@ class BlinkerTransportESP32_BLE
         bool                    deviceConnected;
         char                    BLEBuf[BLINKER_BUFFER_SIZE];
         bool                    isAvail;
+        String                  BLEStr;
         BLEServer               *pServer;
         BLEService              *pService;
         BLECharacteristic       *pCharacteristic;
@@ -126,8 +128,29 @@ class BlinkerTransportESP32_BLE
             std::string value = pCharacteristic->getValue();
 
             if (value.length() > 0) {
-                strcpy(BLEBuf, value.c_str());
-                isAvail = true;
+                // BLEStr += value;
+                char *data;
+                uint8_t len = value.length();
+                data = (char*)malloc((len+1)*sizeof(char));
+                strcpy(data, value.c_str());
+                // BLINKER_LOG2("data: ", String(data));
+                BLEStr += String(data);
+
+                if (STRING_contais_string(BLEStr, BLINKER_CMD_NEWLINE)) {
+                    // BLINKER_LOG2("BLEStr: ", BLEStr);
+                    strcpy(BLEBuf, BLEStr.c_str());
+                    BLINKER_LOG2("BLEBuf: ", BLEBuf);
+                    BLEStr = "";
+                    isAvail = true;
+                }
+
+                // Serial.println("*********");
+                // Serial.print("New value: ");
+                // for (int i = 0; i < value.length(); i++)
+                //     Serial.print(value[i]);
+
+                // Serial.println();
+                // Serial.println("*********");
             }
         }
 };
